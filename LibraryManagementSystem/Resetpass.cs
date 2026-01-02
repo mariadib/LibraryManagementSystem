c7e1dd3641e818dd490d33e4082f0f14f4188041
mariadib <mariadib427@gmail.com>
2026-01-02 22:38:32 +0200

    Forgetting the password form coding and redirecting it to login after resetting it

--- LibraryManagementSystem/Resetpass.cs
+++ LibraryManagementSystem/Resetpass.cs
@@ -7,20 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace LibraryManagementSystem
 {
     public partial class Resetpass : Form
     {
+        static string connString = "Data Source=DESKTOP-HRBJP2J\\SQLEXPRESS;" + "Initial Catalog=LibraryManagementSystem;" + "Integrated Security=True;" + "TrustServerCertificate=True";
+        SqlConnection conn = new SqlConnection(connString);
         public Resetpass()
         {
             InitializeComponent();
         }
-        
+        void Resettingpass()
+        { 
+            string query = $"UPDATE [dbo].[Users] SET password={txt_newpass.Text} WHERE email={txt_email.Text}";
+            SqlCommand cmd = new SqlCommand(query, conn);
 
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery(); 
+                MessageBox.Show("Password has been reset successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close(); 
+            }
+        }
         private void btn_reset_Click(object sender, EventArgs e)
         {
-
+            Resettingpass();
+            Login login = new Login(); 
+            login.Show();                      
+            this.Hide();
         }
     }
 }
